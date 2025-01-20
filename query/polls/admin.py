50b11413915c2e543b3ccc9ee55478c4e21e564e
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-01-20 21:03:19 +0400

    refactor: improved model view in admin.py file

--- query/polls/admin.py
+++ query/polls/admin.py
@@ -1,33 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question, Choice
-from django.utils import timezone
-from django.utils.html import format_html
+from .models import Choice, Question
 
-# Question admin modification
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date', 'was_published_recently')  # Görünən sütunlar
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
-    ordering = ['pub_date'] 
-
-    def was_published_recently(self, obj):
-        return obj.was_published_recently()
-    was_published_recently.boolean = True  
-    was_published_recently.short_description = 'Published recently?' 
-
-# Choice admin modification
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text', 'votes', 'question') 
-    list_filter = ['question'] 
-    search_fields = ['choice_text']  
-    ordering = ['votes']  
-
-    def question_text(self, obj):
-        return obj.question.question_text
-    question_text.short_description = 'Question'  
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Choice)
